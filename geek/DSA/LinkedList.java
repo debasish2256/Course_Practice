07b4566607522e8f5600ace02db46ba92e24edda
Debasish Mahana <debasishmahana49@gmail.com>
2022-04-30 18:16:52 +0530

    Apr23

--- geek/DSA/LinkedList.java
+++ geek/DSA/LinkedList.java
@@ -1,76 +1,82 @@
 package LinkedList;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
-/*"1. Create a linked list and add the following elements to it - 2,4,1,3,8,5
-Search for the number 3 and display its index
+/*"1. Create a linked list and add ten values to it by taking it from the user. 
+Delete all nodes which are greater than 40
 
-Output -> Found at 4th position
+2. Print the node which is at index 5
 
-2. Create a liked list and add the following elements to it - 2,4,1,3,8,5.
-Add the element 20 at the end 
+Input - 10->20->20->40->50->60->70->null
+Output - 50
 
-Output - 2,4,1,3,8,5,20"
+"
 */
 public class LinkedList {
-	static Node head;   
-    public void push(int new_data)
-    {
-        Node new_node = new Node(new_data);
-        new_node.next = head;
-        head = new_node;
-    }
-    public static int search(Node head, int data)
-    {
-        Node current = head; 
-        int count=0;
-        while (current != null)
-        {
-            if (current.data == data)
-                return count;   
-            current = current.next;
-            count++;
-        }
-        return -1;   
-    }
-    private static void addLast(int data) {
-		Node new_Node=new Node(data); 
-        if (head == null) {
-        	return;
-        }else {
-        	Node cur=head;
-        	while(cur.next!=null) {
-        		cur=cur.next;
-        	}
-        	cur.next=new_Node;
-        }
+	static Node head;
+
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		for (int i = 0; i < 10; i++) {
+			ll.push(new Scanner(System.in).nextInt());
+		}
+		/*ll.push(5);
+		ll.push(12);
+
+		ll.push(42);
+
+		ll.push(45);
+		ll.push(15);*/
+
+
+		printList();
+		deleteAllGreaterThan40();
+		printList();
+		System.out.println(printPositionData(3));
+
 	}
-    
-    public static void printlist()
+
+	private static int printPositionData(int pos) {
+		int count = 0;
+		while (head != null) {
+			if (count == pos) {
+				return head.data;
+			}
+			count++;
+			head = head.next;
+		}
+		return -1;
+	}
+	
+	
+	private static void deleteAllGreaterThan40() {
+		if(head!=null) {
+			Node cur=head;
+			while(cur.next!=null) {
+				if(cur.next.data>40) {
+					Node prev=cur;
+					prev.next=cur.next.next;
+				}
+				cur=cur.next;
+			}
+		}
+		
+	}
+
+	public void push(int new_data) {
+		Node new_node = new Node(new_data);
+		new_node.next = head;
+		head = new_node;
+	}
+
+	public static void printList()
     {
-		Node last = null;
-        while (head != null) {
-            System.out.print(head.data + " ");
-            last = head;
-            head = head.next;
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
         }
         System.out.println();
     }
-	
-    public static void main(String args[])
-    {
- 
-    	LinkedList  llist = new LinkedList ();
-        llist.push(10);
-        llist.push(30);
-        llist.push(11);
-        llist.push(21);
-        llist.push(14);
-        System.out.println("Found Position:"+search(llist.head,30));
-        LinkedList.addLast(50);
-        LinkedList.addLast(500);
-        printlist();
 
-    }
-    
 }
